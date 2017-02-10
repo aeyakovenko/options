6ea4a3c8bdf1ba11ee81af1074a5d8a8e72c1d09
Anatoly Yakovenko <aeyakovenko@gmail.com>
2017-02-09 22:46:14 -0800

    update

--- YFO.hs
+++ YFO.hs
@@ -1,78 +1,30 @@
 module YFO where
-
-import qualified Data.Aeson as A
 import qualified Data.ByteString.Lazy as L
-import qualified Data.YFO as Y
-import qualified Data.Option as Y
-import qualified Data.OptionLists as Y
-import qualified Data.Time.Clock as C
-import qualified Data.Time.Clock.POSIX as P
-import qualified Data.Time.Calendar as D
+import Data.List(foldl')
+import Data.Time.Calendar(Day)
 
-import Network.HTTP.Client as H
-import Network.HTTP.Types.Status as H
-
-import Data.Char(toUpper)
-
--- URL = https://query2.finance.yahoo.com/v7/finance/options/SPY?date=1516320000
-
-query :: String -> D.Day -> String 
-query s e = concat ["https://query2.finance.yahoo.com/v7/finance/options/", u, "?date=", t]
-    where t = dayToPosixTime e
-          u = map toUpper s
-
-get :: String -> IO L.ByteString
-get q = do
-  manager <- newManager defaultManagerSettings
-  request <- parseRequest q
-  response <- httpLbs request manager
-  return $ responseBody response
-
-dayToPosixTime :: D.Day -> Integer
-dayToPosixTime = t
-    where t = P.utcTimeToPOSIXSeconds (C.UTCTime e (C.secondsToDiffTime (13 + 60)))
-          u = map toUpper s
-
-today :: IO Integer
-today = map (dayToPosixTime . C.utctDay) t
-    where t = getCurrentTime
-
-parse :: L.ByteString -> Y.OptionChain (Y.Result [Y.OptionsData])
-parse = from . A.eitherDecode
-    where from (Left err) = error err
-          from (Right v) = v
-
-data OptionPrice = Call { expiration :: Integer
-                        , strike :: Double
-                        , price :: Double
-                        }
-                 | Put { expiration :: Integer
-                       , strike :: Double
-                       , price :: Double
-                       }
-                deriving Show
-
-toOptionPrices :: Y.OptionChain (Y.Result [Y.OptionsData]) -> [OptionPrice]
-
-fromOptionsData :: Y.OptionsData -> [OptionPrice]
-
-toOptionPrices (Y.OptionChain (Y.Result ls _)) = concatMap fromOptionsData ls
-
-fromOptionsData (Y.OptionsData {Y.options = ls}) = concatMap fromOptionLists ls
-
-fromOptionLists :: Y.OptionLists -> [OptionPrice]
-fromOptionLists (Y.OptionLists { Y.calls = cs, Y.puts = ps }) = 
-        map (fromOption Call) cs
-     ++ map (fromOption Put) ps
-
-fromOption :: (Integer -> Double -> Double -> OptionPrice) -> Y.Option -> OptionPrice
-fromOption v o = v e s p
-    where e = Y.expiration $ o
-          s = Y.strike o 
-          p = (Y.bid o + Y.ask o)/2
-
+-- symbol,exchange,date,adjusted stock close price,option symbol,expiration,strike,call/put,style,ask,bid,volume,open interest,unadjusted stock price
+data OptionType = Call | Put
+data Quote = Quote { symbol :: String
+                   , exchange :: String
+                   , date :: Day
+                   , adjustedPrice :: Double
+                   , optionSymbol :: String
+                   , expiration :: Day
+                   , strike :: Double
+                   , callOrPut :: OptionType
+                   , style :: String
+                   , ask :: Double
+                   , bid :: Double
+                   , volume :: Double
+                   , openInterest :: Double
+                   , unadjustedStockPrice :: Double
+                   }
+parse ln = 
+    let [sy,ex,da,as,os,exp,st,cp,st,as,bi,vo,oi,us] = splitBy "," ln
+    in Quote sy ex (fromGregorian da) (read as) os (fromGregorian exp) (read st) (toOptionType cp) st (read as) (read bi) (read vo) (read oi) (read us)
 
 main :: IO ()
 main = do
-    f <- L.readFile "data/test.json"
-    print $ toOptionPrices $ parse f
+    lines <- tail <$> L.lines <$> L.readFile "spy_options.1.7.2005.to.12.28.2009.csv"
+    foldl' trade account lines 
